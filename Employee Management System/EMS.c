9706ae4a0c30574fe93a9d51b29ca7002c4ece00
QNasir99 <143062678+QNasir99@users.noreply.github.com>
2023-11-03 21:09:39 +0600

    login and register

--- Employee Management System/EMS.c
+++ Employee Management System/EMS.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
 #include<dos.h>
+#include<conio.h>
 #include"SplashScreen.h"
+#include"Login.h"
 int main()
 {
+    int c;
     SplashScreen();
     sleep(5);
     system("cls");
+    printf("Press 1 to Register\nPress 2 to Login\n");
+    scanf("%d",&c);
+    if(c==1)
+    {
+        system("CLS");
+        regi();
+    }
+    else if(c==2)
+    {
+        system("CLS");
+        login();
+    }
+
     return 0;
 }
---
+++ Employee Management System/Login.h
@@ -0,0 +1,49 @@
+    struct login
+    {
+       char Name[100] ;
+       char Username[20];
+       char Password[20];
+    };
+    regi()
+    {
+        FILE *log;
+        log=fopen("Login.txt","w");
+        struct login l;
+        printf("Enter Name: ");
+        scanf("%s",l.Name);
+        printf("Enter User Name: ");
+        scanf("%s",l.Username);
+        printf("Enter pass word: ");
+        scanf("%s",l.Password);
+        fwrite(&l,sizeof(l),1,log);
+        fclose(log);
+        printf("Registration compleat");
+        printf("\n Press any key to continue");
+        getch();
+        system("CLS");
+        login();
+    }
+    login()
+    {
+        char username[20];
+        char password[20];
+        FILE *log;
+        log=fopen("Login.txt","r");
+        struct login l;
+        printf("Enter User Name: ");
+        scanf("%s",&username);
+        printf("Enter Password: ");
+        scanf("%s",&password);
+        while(fread(&l,sizeof(l),1,log))
+        {
+            if(strcmp(username,l.Username)==0&&strcmp(password,l.Password)==0)
+            {
+                printf("Login Successful");
+            }
+            else
+            {
+                printf("Wrong User Name or Password");
+            }
+        }
+        fclose(log);
+    }
